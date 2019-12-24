971b72850dbd543ab805f782333afdb143ef8882
bknutson123 <bentknutson@gmail.com>
2019-12-24 11:21:07 -0700

    upgraded acs version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 //TODO: maybe move to single repo using github pages
 module "acs" {
-  source = "github.com/byu-oit/terraform-aws-acs-info.git?ref=v1.0.4"
+  source = "github.com/byu-oit/terraform-aws-acs-info.git?ref=v1.1.0"
   env = var.env
   vpc_vpn_to_campus = true
 }
@@ -15,16 +15,13 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:us-west-2:${var.account-id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:us-west-2:${var.account-id}:${aws_api_gateway_rest_api.api.id}/*"
 }
-
-
-
 
 
 resource "aws_api_gateway_domain_name" "api_domain" {
   certificate_arn = "${data.aws_ssm_parameter.us-east-1-cert.arn}"
-  domain_name     = "${var.dns-name}.${module.acs.route53_zone.zone_name}"
+  domain_name     = "${var.dns-name}.${module.acs.route53_zone.name}"
 }
 
 resource "aws_route53_record" "a_record" {
