478d3d8dd0abf77fe7ebf05e48b0acf05d547d90
bknutson123 <bentknutson@gmail.com>
2019-12-24 10:54:51 -0700

    removed route 53 record

--- main.tf
+++ main.tf
@@ -9,28 +9,6 @@ resource "aws_api_gateway_rest_api" "api" {
   name = "${var.app-name}-api"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda.invoke_arn
-}
-
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -44,24 +22,23 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
 
 
-
-resource "aws_api_gateway_domain_name" "api_domain" {
-  certificate_arn = "${data.aws_ssm_parameter.us-east-1-cert.arn}"
-  domain_name     = "${var.dns-name}.${module.acs.route53_zone.zone_id}"
-}
-
-resource "aws_route53_record" "a_record" {
-  name    = aws_api_gateway_domain_name.api_domain.domain_name
-  type    = "A"
-  zone_id = module.acs.route53_zone.zone_id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
-  }
-}
-
+//resource "aws_api_gateway_domain_name" "api_domain" {
+//  certificate_arn = "${data.aws_ssm_parameter.us-east-1-cert.arn}"
+//  domain_name     = "${var.dns-name}.${module.acs.route53_zone.zone_id}"
+//}
+//
+//resource "aws_route53_record" "a_record" {
+//  name    = aws_api_gateway_domain_name.api_domain.domain_name
+//  type    = "A"
+//  zone_id = module.acs.route53_zone.zone_id
+//
+//  alias {
+//    evaluate_target_health = true
+//    name                   = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
+//    zone_id                = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
+//  }
+//}
+//
 //resource "aws_route53_record" "aaaa_record" {
 //  name    = aws_api_gateway_domain_name.api_domain.domain_name
 //  type    = "AAAA"
