ccf35b184585e66c00964f40473360dcd5637af2
bknutson123 <bentknutson@gmail.com>
2019-12-24 17:32:13 -0700

    added count to two other resources

--- main.tf
+++ main.tf
@@ -13,16 +13,18 @@ resource "aws_api_gateway_resource" "resource" {
 }
 
 resource "aws_api_gateway_method" "method" {
+  count = "${length(var.method-paths)}"
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = "${aws_api_gateway_resource.resource[count.index].id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = module.api_gateway_and_lambda.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  count = "${length(var.method-paths)}"
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = "${aws_api_gateway_resource.resource[count.index].id}"
+  http_method             = "${aws_api_gateway_method.method[count.index].http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
