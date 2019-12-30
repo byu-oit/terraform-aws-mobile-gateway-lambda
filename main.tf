cd47b8144e330af073bbef0fe8e2190a18ffb8da
bknutson123 <bentknutson@gmail.com>
2019-12-30 11:06:26 -0700

    fix :

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_api_gateway_integration" "root_method_integration" {
 
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_rest_api.api.root_resource_id
-  http_method             = aws_api_gateway_method.root_method.http_method
+  http_method             = var.root-resource-method
   integration_http_method = var.root-resource-method
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
